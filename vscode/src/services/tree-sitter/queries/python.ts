cc094f3f7d6e3996f2337486ff65736cb3d72b88
iohub <rickyang.pro@gmail.com>
2026-08-16 10:57:25 +0800

    remove vscode & webui

--- vscode/src/services/tree-sitter/queries/python.ts
+++
@@ -1,7 +0,0 @@
-export default `
-(class_definition
-  name: (identifier) @name.definition.class) @definition.class
-
-(function_definition
-  name: (identifier) @name.definition.function) @definition.function
-`
